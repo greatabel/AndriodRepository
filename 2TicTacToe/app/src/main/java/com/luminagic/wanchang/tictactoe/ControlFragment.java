2e60a79cbc5a6c4a5aa34b16d6a921575aea1c4a
greatabel <myreceiver2for2github@gmail.com>
2016-04-16 16:44:23 +0800

    change ui

---
+++ 2TicTacToe/app/src/main/java/com/luminagic/wanchang/tictactoe/ControlFragment.java
@@ -0,0 +1,14 @@
+package com.luminagic.wanchang.tictactoe;
+
+//import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ControlFragment extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
