d3725723d48855a9536fa7cd215b37c31a381bde
greatabel <myreceiver2for2github@gmail.com>
2016-05-05 23:48:22 +0800

    ui ch10

--- 10ConnectingtotheWorld/app/src/main/java/com/luminagic/wanchang/a10connectingtotheworld/MainActivity.java
+++ 10ConnectingtotheWorld/app/src/main/java/com/luminagic/wanchang/a10connectingtotheworld/MainActivity.java
@@ -1,9 +1,26 @@
 package com.luminagic.wanchang.a10connectingtotheworld;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+//import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
+
+public class MainActivity extends Activity {
+
+    private EditText urlText;
+    private Button goButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
