62c83ccde3e0997098be69d4ba833c512e2ad034
greatabel <myreceiver2for2github@gmail.com>
2016-05-10 22:47:07 +0800

    url opener

--- 10ConnectingtotheWorld/app/src/main/java/com/luminagic/wanchang/a10connectingtotheworld/MainActivity.java
+++ 10ConnectingtotheWorld/app/src/main/java/com/luminagic/wanchang/a10connectingtotheworld/MainActivity.java
@@ -26,6 +26,37 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get a handle to all user interface elements
+        urlText = (EditText)findViewById(R.id.url_field);
+        goButton = (Button)findViewById(R.id.go_button);
+
+        goButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               openBrowser();
+            }
+        });
+        urlText.setOnEditorActionListener(new OnEditorActionListener() {
+            public boolean onEditorAction(TextView v, int actionId,
+                                          KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_GO) {
+                    openBrowser();
+                    InputMethodManager imm = (InputMethodManager)
+                            getSystemService(INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                    return true;
+                }
+                return false;
+            }
+        });
+
         
+    }
+
+    private void openBrowser() {
+        Uri uri = Uri.parse(urlText.getText().toString());
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
     }
 }
