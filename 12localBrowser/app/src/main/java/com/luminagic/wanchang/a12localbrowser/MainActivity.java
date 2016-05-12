02225b2e671aaae1f9dc41669b2f1c88f5b5ab11
greatabel <myreceiver2for2github@gmail.com>
2016-05-12 23:12:54 +0800

    12

--- 12localBrowser/app/src/main/java/com/luminagic/wanchang/a12localbrowser/MainActivity.java
+++ 12localBrowser/app/src/main/java/com/luminagic/wanchang/a12localbrowser/MainActivity.java
@@ -1,9 +1,26 @@
 package com.luminagic.wanchang.a12localbrowser;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.webkit.JavascriptInterface;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private static final String TAG = "LocalBrowser";
+    private final Handler handler = new Handler();
+    private WebView webView;
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
