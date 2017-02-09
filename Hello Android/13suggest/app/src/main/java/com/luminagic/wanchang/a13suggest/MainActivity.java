62b252b97160eff06632e2ef6907cc457b7cfd71
greatabel <myreceiver2for2github@gmail.com>
2017-02-09 16:04:54 +0800

    compact

--- Hello Android/13suggest/app/src/main/java/com/luminagic/wanchang/a13suggest/MainActivity.java
+++ Hello Android/13suggest/app/src/main/java/com/luminagic/wanchang/a13suggest/MainActivity.java
@@ -1,9 +1,30 @@
 package com.luminagic.wanchang.a13suggest;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText origText;
+    private ListView suggList;
+    private TextView ebandText;
+
+    private Handler guiThread;
+    private ExecutorService suggThread;
+    private Runnable updateTask;
+    private Future<?> suggPending;
+    private List<String> items;
+    private ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
