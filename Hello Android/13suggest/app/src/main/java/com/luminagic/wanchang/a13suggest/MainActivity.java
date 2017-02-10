9a4710235a0bd86a66c55d724977a15ad53af9e0
greatabel <myreceiver2for2github@gmail.com>
2017-02-10 10:58:03 +0800

    add listener

--- Hello Android/13suggest/app/src/main/java/com/luminagic/wanchang/a13suggest/MainActivity.java
+++ Hello Android/13suggest/app/src/main/java/com/luminagic/wanchang/a13suggest/MainActivity.java
@@ -3,11 +3,14 @@ package com.luminagic.wanchang.a13suggest;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
@@ -43,7 +46,9 @@ public class MainActivity extends AppCompatActivity {
      * and 2) The suggest thread, which we start using an executor.
      */
     private void initThreading() {
-
+        origText = (EditText)findViewById(R.id.original_text);
+        suggList = (ListView)findViewById(R.id.result_list);
+        ebandText = (TextView)findViewById(R.id.eband_text);
     }
 
     private void findViews() {
@@ -51,10 +56,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setListeners() {
+     // Define listener for text change
+        TextWatcher textWatcher = new TextWatcher() {
 
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            public void afterTextChanged(Editable editable) {
+
+            }
+        };
     }
     private void setAdapters() {
-        
+
+        items = new ArrayList<String>();
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                items);
+        suggList.setAdapter(adapter);
     }
 
 }
