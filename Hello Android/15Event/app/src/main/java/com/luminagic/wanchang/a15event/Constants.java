ea38fb490218edd28e2559071deee7a9e5a7548a
greatabel <myreceiver2for2github@gmail.com>
2017-02-13 11:02:56 +0800

    EventsData

---
+++ Hello Android/15Event/app/src/main/java/com/luminagic/wanchang/a15event/Constants.java
@@ -0,0 +1,13 @@
+package com.luminagic.wanchang.a15event;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by wanchang on 2017/2/13.
+ */
+public interface Constants extends BaseColumns {
+    public  static final String TABLE_NAME = "events";
+
+    public  static final String TIME = "time";
+    public  static final String TITLE = "title";
+}
