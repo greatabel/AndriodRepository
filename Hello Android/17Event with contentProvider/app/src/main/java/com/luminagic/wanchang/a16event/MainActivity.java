56d760adc70a16d3ebfd827c53576b119541da03
greatabel <myreceiver2for2github@gmail.com>
2017-02-13 17:58:27 +0800

    17 final version

--- Hello Android/17Event with contentProvider/app/src/main/java/com/luminagic/wanchang/a16event/MainActivity.java
+++ Hello Android/17Event with contentProvider/app/src/main/java/com/luminagic/wanchang/a16event/MainActivity.java
@@ -3,20 +3,14 @@ package com.luminagic.wanchang.a16event;
 import android.app.ListActivity;
 import android.content.ContentValues;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.SimpleCursorAdapter;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.luminagic.wanchang.a15event.R;
 
 import static android.provider.BaseColumns._ID;
-import static com.luminagic.wanchang.a16event.Constants.TABLE_NAME;
+import static com.luminagic.wanchang.a16event.Constants.CONTENT_URI;
 import static com.luminagic.wanchang.a16event.Constants.TIME;
 import static com.luminagic.wanchang.a16event.Constants.TITLE;
 
@@ -25,7 +19,7 @@ public class MainActivity extends ListActivity {
 
     private static String[] FROM = {_ID, TIME, TITLE,};
     private static String ORDER_BY = TIME + " DESC";
-    private EventsData events;
+//    private EventsData events;
 
     private static int[] TO = {R.id.rowid, R.id.time, R.id.title,};
     /**
@@ -39,41 +33,34 @@ public class MainActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        events = new EventsData(this);
-        int i = 0;
-        try {
-            Log.w("log", "i= " + (++i));
-
-            addEvent("Hello, Android from abel");
-            Cursor cursor = getEvents();
-            showEvents(cursor);
-        } finally {
-            events.close();
-        }
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        addEvent("Hello, Android!");
+        Cursor cursor = getEvents();
+        showEvents(cursor);
     }
 
     private void addEvent(String string) {
         // Insert a new record into the Events data source.
         // You would do something similar for delete and update.
-        SQLiteDatabase db = events.getWritableDatabase();
+//        SQLiteDatabase db = events.getWritableDatabase();
+//        ContentValues values = new ContentValues();
+//        values.put(TIME, System.currentTimeMillis());
+//        values.put(TITLE, string);
+//        db.insertOrThrow(TABLE_NAME, null, values);
         ContentValues values = new ContentValues();
         values.put(TIME, System.currentTimeMillis());
         values.put(TITLE, string);
-        db.insertOrThrow(TABLE_NAME, null, values);
+        getContentResolver().insert(CONTENT_URI, values);
     }
 
     private Cursor getEvents() {
         // Perform a managed query. The Activity will handle closing
         // and re-querying the cursor when needed.
-        SQLiteDatabase db = events.getReadableDatabase();
-        Cursor cursor = db.query(TABLE_NAME, FROM, null, null, null,
-                null, ORDER_BY);
-        startManagingCursor(cursor);
-        return cursor;
+//        SQLiteDatabase db = events.getReadableDatabase();
+//        Cursor cursor = db.query(TABLE_NAME, FROM, null, null, null,
+//                null, ORDER_BY);
+//        startManagingCursor(cursor);
+//        return cursor;
+        return managedQuery(CONTENT_URI, FROM, null,null, ORDER_BY);
     }
 
     //    private void showEvents(Cursor cursor){
@@ -99,39 +86,5 @@ public class MainActivity extends ListActivity {
         setListAdapter(adapter);
     }
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Main Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
-    }
 }
