02297ebfc3e0bea5d7a4063b19f971d447805ae3
greatabel <myreceiver2for2github@gmail.com>
2017-02-13 17:44:41 +0800

    change constants

--- Hello Android/17Event with contentProvider/app/src/main/java/com/luminagic/wanchang/a16event/Constants.java
+++ Hello Android/17Event with contentProvider/app/src/main/java/com/luminagic/wanchang/a16event/Constants.java
@@ -1,5 +1,6 @@
 package com.luminagic.wanchang.a16event;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -7,6 +8,10 @@ import android.provider.BaseColumns;
  */
 public interface Constants extends BaseColumns {
     public  static final String TABLE_NAME = "events";
+
+    public static final String AUTHORITY = "com.luminagic.wanchang.a16event";
+    public static final Uri CONTENT_URI = Uri.parse("content://"
+            + AUTHORITY + "/" + TABLE_NAME);
 
     public  static final String TIME = "time";
     public  static final String TITLE = "title";
