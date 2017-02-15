7c2cc5899f84aa12aa1f09dcc146c9163d64574a
greatabel <myreceiver2for2github@gmail.com>
2017-02-15 15:52:13 +0800

    01GEO_Quiz

---
+++ Android Programming The big Nerd Ranch/01GeoQuiz/app/src/main/java/com/luminagic/wanchang/a01geoquiz/QuizActivity.java
@@ -0,0 +1,13 @@
+package com.luminagic.wanchang.a01geoquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+}
