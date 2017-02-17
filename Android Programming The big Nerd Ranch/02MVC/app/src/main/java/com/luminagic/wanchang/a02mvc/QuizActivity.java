8895d2cb91e44e8eea95cde823a4f8c86c5cd866
greatabel <myreceiver2for2github@gmail.com>
2017-02-17 17:01:43 +0800

    make question and answer match

--- Android Programming The big Nerd Ranch/02MVC/app/src/main/java/com/luminagic/wanchang/a02mvc/QuizActivity.java
+++ Android Programming The big Nerd Ranch/02MVC/app/src/main/java/com/luminagic/wanchang/a02mvc/QuizActivity.java
@@ -23,6 +23,18 @@ public class QuizActivity extends AppCompatActivity {
             new Question(R.string.question_asia, true)
     };
 
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+        int messageResId = 0;
+        if(userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+                .show();
+    }
+
     private  int mCurrentIndex = 0;
 
     private  void updateQuestion() {
@@ -44,17 +56,19 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener(){
          @Override
          public void onClick(View v){
-             Toast.makeText(QuizActivity.this,
-                     R.string.correct_toast,
-                     Toast.LENGTH_SHORT).show();
+//             Toast.makeText(QuizActivity.this,
+//                     R.string.correct_toast,
+//                     Toast.LENGTH_SHORT).show();
+             checkAnswer(true);
          }
         });
         mFalseButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Toast.makeText(QuizActivity.this,
-                        R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
+//                Toast.makeText(QuizActivity.this,
+//                        R.string.incorrect_toast,
+//                        Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
 
