416b10f12027a729135c46808a4f580e74474717
greatabel <myreceiver2for2github@gmail.com>
2017-02-17 16:57:54 +0800

    extract same logic code to common funct

--- Android Programming The big Nerd Ranch/02MVC/app/src/main/java/com/luminagic/wanchang/a02mvc/QuizActivity.java
+++ Android Programming The big Nerd Ranch/02MVC/app/src/main/java/com/luminagic/wanchang/a02mvc/QuizActivity.java
@@ -1,8 +1,7 @@
 package com.luminagic.wanchang.a02mvc;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -26,15 +25,18 @@ public class QuizActivity extends AppCompatActivity {
 
     private  int mCurrentIndex = 0;
 
+    private  void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
-        int question = mQuestionBank[mCurrentIndex].getTextResId();
-        Log.d("question:-> ", Integer.toString(question));
-        mQuestionTextView.setText(question);
+
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalseButton = (Button) findViewById(R.id.false_button);
@@ -61,10 +63,11 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 mCurrentIndex = (mCurrentIndex + 1)% mQuestionBank.length;
-                int question = mQuestionBank[mCurrentIndex].getTextResId();
-                mQuestionTextView.setText(question);
+                updateQuestion();
             }
         });
+
+        updateQuestion();
 
     }
 }
