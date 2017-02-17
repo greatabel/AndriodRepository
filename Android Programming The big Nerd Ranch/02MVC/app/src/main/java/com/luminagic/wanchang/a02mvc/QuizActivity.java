415b3931be67dd30e8b4df94b88d5bf0205ac772
greatabel <myreceiver2for2github@gmail.com>
2017-02-17 16:53:57 +0800

    change question change

--- Android Programming The big Nerd Ranch/02MVC/app/src/main/java/com/luminagic/wanchang/a02mvc/QuizActivity.java
+++ Android Programming The big Nerd Ranch/02MVC/app/src/main/java/com/luminagic/wanchang/a02mvc/QuizActivity.java
@@ -56,5 +56,15 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
+        mNextButton = (Button)findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mCurrentIndex = (mCurrentIndex + 1)% mQuestionBank.length;
+                int question = mQuestionBank[mCurrentIndex].getTextResId();
+                mQuestionTextView.setText(question);
+            }
+        });
+
     }
 }
