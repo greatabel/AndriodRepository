46f127608de10792d9068318d5949b9cd58531bf
greatabel <myreceiver2for2github@gmail.com>
2017-02-17 17:02:45 +0800

    rm uncessary code

--- Android Programming The big Nerd Ranch/02MVC/app/src/main/java/com/luminagic/wanchang/a02mvc/QuizActivity.java
+++ Android Programming The big Nerd Ranch/02MVC/app/src/main/java/com/luminagic/wanchang/a02mvc/QuizActivity.java
@@ -56,18 +56,14 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener(){
          @Override
          public void onClick(View v){
-//             Toast.makeText(QuizActivity.this,
-//                     R.string.correct_toast,
-//                     Toast.LENGTH_SHORT).show();
+
              checkAnswer(true);
          }
         });
         mFalseButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-//                Toast.makeText(QuizActivity.this,
-//                        R.string.incorrect_toast,
-//                        Toast.LENGTH_SHORT).show();
+
                 checkAnswer(false);
             }
         });
