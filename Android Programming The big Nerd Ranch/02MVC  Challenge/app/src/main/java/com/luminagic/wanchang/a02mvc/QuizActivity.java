733def17ea2e1f89742f98726272f897ef846f65
greatabel <myreceiver2for2github@gmail.com>
2017-02-20 10:42:05 +0800

    prev button func

--- Android Programming The big Nerd Ranch/02MVC  Challenge/app/src/main/java/com/luminagic/wanchang/a02mvc/QuizActivity.java
+++ Android Programming The big Nerd Ranch/02MVC  Challenge/app/src/main/java/com/luminagic/wanchang/a02mvc/QuizActivity.java
@@ -14,6 +14,7 @@ public class QuizActivity extends AppCompatActivity {
     private Button mFalseButton;
 
     private Button mNextButton;
+    private Button mPrevButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[] {
@@ -84,6 +85,19 @@ public class QuizActivity extends AppCompatActivity {
             public void onClick(View v){
                 mCurrentIndex = (mCurrentIndex + 1)% mQuestionBank.length;
                 updateQuestion();
+            }
+        });
+
+        mPrevButton = (Button)findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View v){
+                mCurrentIndex = (mCurrentIndex - 1)% mQuestionBank.length;
+                if(mCurrentIndex < 0 && mCurrentIndex >= -4){
+                    mCurrentIndex += mQuestionBank.length;
+                }
+                Log.d("mPrevButton", Integer.toString(mCurrentIndex));
+//                updateQuestion();
             }
         });
 
