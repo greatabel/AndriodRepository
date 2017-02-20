5e96d036c933301f3579a121745e7934603b9dd0
greatabel <myreceiver2for2github@gmail.com>
2017-02-20 11:25:44 +0800

    03 Lifecycle

---
+++ Android Programming The big Nerd Ranch/03 Activity Lifecycle/app/src/main/java/com/luminagic/wanchang/a02mvc/QuizActivity.java
@@ -0,0 +1,108 @@
+package com.luminagic.wanchang.a02mvc;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+
+    private ImageButton mNextButton;
+    private ImageButton mPrevButton;
+    private TextView mQuestionTextView;
+
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true)
+    };
+
+    public void onClickTextView(View v) {
+    // add click event for textview(question)
+
+        mCurrentIndex = (mCurrentIndex + 1)% mQuestionBank.length;
+        Log.d("onClickTextView:", Integer.toString(mCurrentIndex));
+        updateQuestion();
+
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+        int messageResId = 0;
+        if(userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    private  int mCurrentIndex = 0;
+
+    private  void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener(){
+         @Override
+         public void onClick(View v){
+
+             checkAnswer(true);
+         }
+        });
+        mFalseButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                checkAnswer(false);
+            }
+        });
+
+        mNextButton = (ImageButton)findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mCurrentIndex = (mCurrentIndex + 1)% mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        mPrevButton = (ImageButton)findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View v){
+                mCurrentIndex = (mCurrentIndex - 1)% mQuestionBank.length;
+                if(mCurrentIndex < 0 && mCurrentIndex >= -4){
+                    mCurrentIndex += mQuestionBank.length;
+                }
+                Log.d("mPrevButton", Integer.toString(mCurrentIndex));
+                updateQuestion();
+            }
+        });
+
+        updateQuestion();
+
+    }
+}
