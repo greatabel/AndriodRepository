1ff8ccc75577ded9dae569e123f106b8b79ffb06
greatabel <myreceiver2for2github@gmail.com>
2017-03-07 16:19:28 +0800

    add animation to hide button ,but only in api.version >=21

--- Android Programming The big Nerd Ranch/06SDK Versions/app/src/main/java/com/luminagic/wanchang/a02mvc/CheatActivity.java
+++ Android Programming The big Nerd Ranch/06SDK Versions/app/src/main/java/com/luminagic/wanchang/a02mvc/CheatActivity.java
@@ -1,16 +1,22 @@
 package com.luminagic.wanchang.a02mvc;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class CheatActivity extends AppCompatActivity {
 
+    private static final String TAG = "CheatActivity";
     private static final String EXTRA_ANSWER_IS_TRUE =
             "com.luminagic.wanchang.a02mvc.answer_is_true";
 
@@ -46,6 +52,30 @@ public class CheatActivity extends AppCompatActivity {
                     mAnswerTextView.setText(R.string.false_button);
                 }
                 setAnswerShownResult(true);
+
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswer.getWidth() / 2;
+                    int cy = mShowAnswer.getHeight() / 2;
+                    float radius = mShowAnswer.getWidth();
+                    Animator anim = ViewAnimationUtils
+                            .createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mAnswerTextView.setVisibility(View.VISIBLE);
+                            mShowAnswer.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    anim.start();
+
+                    Log.i(TAG,Integer.toString(Build.VERSION.SDK_INT));
+
+                } else {
+                    mShowAnswer.setVisibility(View.INVISIBLE);
+                    Log.i(TAG,"else onClick");
+                }
+
             }
         });
 
