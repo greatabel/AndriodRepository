747225fc7549d68eb0e556fd9cd00dedfeeb0903
greatabel <myreceiver2for2github@gmail.com>
2017-03-08 10:53:04 +0800

    add fragment

--- Android Programming The big Nerd Ranch/07CriminalIntent/app/src/main/java/com/example/wanchang/a07criminalintent/CrimeFragment.java
+++ Android Programming The big Nerd Ranch/07CriminalIntent/app/src/main/java/com/example/wanchang/a07criminalintent/CrimeFragment.java
@@ -1,6 +1,6 @@
 package com.example.wanchang.a07criminalintent;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
