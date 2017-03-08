8ed10ebc2c1ea66ec1e24dc72e1ce313db8aca3d
greatabel <myreceiver2for2github@gmail.com>
2017-03-08 15:17:24 +0800

    crimeFragment

--- Android Programming The big Nerd Ranch/08Fragmentlayout/app/src/main/java/com/example/wanchang/a08fragment_layout/CrimeFragment.java
+++ Android Programming The big Nerd Ranch/08Fragmentlayout/app/src/main/java/com/example/wanchang/a08fragment_layout/CrimeFragment.java
@@ -1,9 +1,31 @@
 package com.example.wanchang.a08fragment_layout;
 
 import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
 
 
 public class CrimeFragment extends Fragment {
-    
+    private Crime mCrime;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        return v;
+
+    }
 }
