12879e11e17cb4e2f2285389ae70c475dece4c9a
greatabel <myreceiver2for2github@gmail.com>
2017-03-10 16:02:49 +0800

    add abstract class

---
+++ Android Programming The big Nerd Ranch/09RecycerView to show list/app/src/main/java/com/example/wanchang/a08fragment_layout/SingleFragmentActivity.java
@@ -0,0 +1,8 @@
+package com.example.wanchang.a08fragment_layout;
+
+
+import android.support.v4.app.FragmentActivity;
+
+public abstract class SingleFragmentActivity extends FragmentActivity{
+    
+}
