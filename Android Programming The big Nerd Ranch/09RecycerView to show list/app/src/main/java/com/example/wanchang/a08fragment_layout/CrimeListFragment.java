e38914f419863933dddc3a1cdf4407fad9607b76
greatabel <myreceiver2for2github@gmail.com>
2017-03-10 16:49:41 +0800

    new laucher

---
+++ Android Programming The big Nerd Ranch/09RecycerView to show list/app/src/main/java/com/example/wanchang/a08fragment_layout/CrimeListFragment.java
@@ -0,0 +1,9 @@
+package com.example.wanchang.a08fragment_layout;
+
+import android.support.v4.app.Fragment;
+
+
+
+public class CrimeListFragment extends Fragment {
+
+}
--- Android Programming The big Nerd Ranch/09RecycerView to show list/app/src/main/java/com/example/wanchang/a08fragment_layout/SingleFragmentActivity.java
+++ Android Programming The big Nerd Ranch/09RecycerView to show list/app/src/main/java/com/example/wanchang/a08fragment_layout/SingleFragmentActivity.java
@@ -6,19 +6,19 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 
-public abstract class SingleFragmentActivity extends FragmentActivity{
+public abstract class SingleFragmentActivity extends FragmentActivity {
 
-    protected  abstract Fragment createFragment();
+    protected abstract Fragment createFragment();
 
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
-        if(fragment == null){
+        if (fragment == null) {
             fragment = createFragment();
             fm.beginTransaction()
                     .add(R.id.fragment_container, fragment)
