7a1627ae16ead541893e8590fca137701b5d51e7
greatabel <myreceiver2for2github@gmail.com>
2017-03-10 15:52:16 +0800

    arraylist of Crimes

--- Android Programming The big Nerd Ranch/09RecycerView to show list/app/src/main/java/com/example/wanchang/a08fragment_layout/CrimeLab.java
+++ Android Programming The big Nerd Ranch/09RecycerView to show list/app/src/main/java/com/example/wanchang/a08fragment_layout/CrimeLab.java
@@ -3,8 +3,33 @@ package com.example.wanchang.a08fragment_layout;
 
 import android.content.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class CrimeLab {
     private  static CrimeLab sCrimeLab;
+
+    public List<Crime> getmCrimes() {
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id){
+        for(Crime crime: mCrimes){
+            if(crime.getmId().equals(id)){
+                return crime;
+            }
+        }
+        return  null;
+    }
+
+    public void setmCrimes(List<Crime> mCrimes) {
+        this.mCrimes = mCrimes;
+    }
+
+
+
+    private List<Crime> mCrimes;
 
     public static CrimeLab get(Context context) {
         if(sCrimeLab == null) {
@@ -14,7 +39,8 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context) {
-        
+        mCrimes = new ArrayList<>();
     }
+
 
 }
