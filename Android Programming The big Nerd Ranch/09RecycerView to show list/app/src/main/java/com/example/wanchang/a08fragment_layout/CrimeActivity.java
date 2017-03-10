b673c140efbaf8e8cb58b36657ba1f106e3fb577
greatabel <myreceiver2for2github@gmail.com>
2017-03-10 16:00:06 +0800

    rename layout

--- Android Programming The big Nerd Ranch/09RecycerView to show list/app/src/main/java/com/example/wanchang/a08fragment_layout/CrimeActivity.java
+++ Android Programming The big Nerd Ranch/09RecycerView to show list/app/src/main/java/com/example/wanchang/a08fragment_layout/CrimeActivity.java
@@ -3,7 +3,6 @@ package com.example.wanchang.a08fragment_layout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CrimeActivity extends FragmentActivity {
@@ -11,7 +10,7 @@ public class CrimeActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
+        setContentView(R.layout.activity_fragment);
 
         FragmentManager fm = getSupportFragmentManager();
 
