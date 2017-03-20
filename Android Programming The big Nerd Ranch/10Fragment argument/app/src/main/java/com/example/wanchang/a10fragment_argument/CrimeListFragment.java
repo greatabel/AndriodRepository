85a6bbc252a052cbdea77a4fe93342f93c23ab37
greatabel <myreceiver2for2github@gmail.com>
2017-03-20 14:37:04 +0800

    update list

--- Android Programming The big Nerd Ranch/10Fragment argument/app/src/main/java/com/example/wanchang/a10fragment_argument/CrimeListFragment.java
+++ Android Programming The big Nerd Ranch/10Fragment argument/app/src/main/java/com/example/wanchang/a10fragment_argument/CrimeListFragment.java
@@ -51,12 +51,28 @@ public class CrimeListFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateUI();
+    }
+
+
     private void updateUI() {
 //        CrimeLab crimeLab = CrimeLab.get(getActivity());
 //        List<Crime> crimes = crimeLab.getmCrimes();
 
-        mAdapter = new CrimeAdapter(crimes);
-        mCrimeRecyclerView.setAdapter(mAdapter);
+//        mAdapter = new CrimeAdapter(crimes);
+//        mCrimeRecyclerView.setAdapter(mAdapter);
+        CrimeLab crimeLab = CrimeLab.get(getActivity());
+        List<Crime> crimes = crimeLab.getmCrimes();
+
+        if(mAdapter == null){
+            mAdapter = new CrimeAdapter(crimes);
+            mCrimeRecyclerView.setAdapter(mAdapter);
+        } else {
+            mAdapter.notifyDataSetChanged();
+        }
 
     }
 
