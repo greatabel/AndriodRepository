18f7def36aab1bb694cc2aff2e95ef30c91e87f5
greatabel <myreceiver2for2github@gmail.com>
2017-03-22 11:19:47 +0800

    11ViewPager

---
+++ Android Programming The big Nerd Ranch/11ViewPager/app/src/main/java/com/example/wanchang/a10fragment_argument/CrimePagerActivity.java
@@ -0,0 +1,19 @@
+package com.example.wanchang.a10fragment_argument;
+
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import android.support.v4.view.ViewPager;
+
+import com.example.wanchang.a08fragment_layout.R;
+
+
+public class CrimePagerActivity extends FragmentActivity {
+
+     @Override
+     protected void onCreate(Bundle savedInstanceState){
+         super.onCreate(savedInstanceState);
+         setContentView(R.layout.activity_crime_pager);
+     }
+ }
