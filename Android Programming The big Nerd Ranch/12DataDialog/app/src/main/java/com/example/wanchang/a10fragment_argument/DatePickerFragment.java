1d8f9bffabc698992c10cc25a292857e283b1c6e
greatabel <myreceiver2for2github@gmail.com>
2017-03-22 21:01:29 +0800

    dialog_date.xml

--- Android Programming The big Nerd Ranch/12DataDialog/app/src/main/java/com/example/wanchang/a10fragment_argument/DatePickerFragment.java
+++ Android Programming The big Nerd Ranch/12DataDialog/app/src/main/java/com/example/wanchang/a10fragment_argument/DatePickerFragment.java
@@ -5,6 +5,8 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
 
 
 import com.example.wanchang.a08fragment_layout.R;
@@ -12,7 +14,12 @@ import com.example.wanchang.a08fragment_layout.R;
 public class DatePickerFragment extends DialogFragment{
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_date, null);
+
         return new android.support.v7.app.AlertDialog.Builder(getActivity())
+                .setView(v)
                 .setTitle(R.string.date_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
