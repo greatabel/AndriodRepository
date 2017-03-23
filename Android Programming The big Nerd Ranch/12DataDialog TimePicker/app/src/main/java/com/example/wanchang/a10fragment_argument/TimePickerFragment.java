53a36679b320f9f0ab45ed9839fbbf87018f9fbc
greatabel <myreceiver2for2github@gmail.com>
2017-03-23 17:57:20 +0800

    12DataDialog

---
+++ Android Programming The big Nerd Ranch/12DataDialog TimePicker/app/src/main/java/com/example/wanchang/a10fragment_argument/TimePickerFragment.java
@@ -0,0 +1,9 @@
+package com.example.wanchang.a10fragment_argument;
+import android.support.v4.app.DialogFragment;
+import android.widget.TimePicker;
+
+public class TimePickerFragment extends DialogFragment {
+    
+    private TimePicker mTimePicker;
+
+}
