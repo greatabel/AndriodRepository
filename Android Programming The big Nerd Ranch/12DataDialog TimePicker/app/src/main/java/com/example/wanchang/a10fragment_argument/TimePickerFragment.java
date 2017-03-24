2c5414a9b43c7288a1571a012d9a8e9b225b6bc5
greatabel <myreceiver2for2github@gmail.com>
2017-03-24 12:58:50 +0800

    timePicker layout and invoke

--- Android Programming The big Nerd Ranch/12DataDialog TimePicker/app/src/main/java/com/example/wanchang/a10fragment_argument/TimePickerFragment.java
+++ Android Programming The big Nerd Ranch/12DataDialog TimePicker/app/src/main/java/com/example/wanchang/a10fragment_argument/TimePickerFragment.java
@@ -3,6 +3,8 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.TimePicker;
 
 import com.example.wanchang.a08fragment_layout.R;
@@ -13,7 +15,12 @@ public class TimePickerFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_time, null);
+
         return new AlertDialog.Builder(getActivity())
+                .setView(v)
                 .setTitle(R.string.time_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
