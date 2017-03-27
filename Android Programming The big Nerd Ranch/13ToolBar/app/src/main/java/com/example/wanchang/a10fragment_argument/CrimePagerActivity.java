74d88a7339155fd99b01eb5c7399f248d34e281e
greatabel <myreceiver2for2github@gmail.com>
2017-03-27 14:06:46 +0800

    13Toolbar

--- Android Programming The big Nerd Ranch/13ToolBar/app/src/main/java/com/example/wanchang/a10fragment_argument/CrimePagerActivity.java
+++ Android Programming The big Nerd Ranch/13ToolBar/app/src/main/java/com/example/wanchang/a10fragment_argument/CrimePagerActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.wanchang.a08fragment_layout.R;
 
@@ -17,7 +18,7 @@ import java.util.List;
 import java.util.UUID;
 
 
-public class CrimePagerActivity extends FragmentActivity {
+public class CrimePagerActivity extends AppCompatActivity {
 
     private static final String EXTRA_CRIME_ID =
             "com.example.wanchang.a10fragment_argument.crime_id";
--- Android Programming The big Nerd Ranch/13ToolBar/app/src/main/java/com/example/wanchang/a10fragment_argument/SingleFragmentActivity.java
+++ Android Programming The big Nerd Ranch/13ToolBar/app/src/main/java/com/example/wanchang/a10fragment_argument/SingleFragmentActivity.java
@@ -5,10 +5,11 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.wanchang.a08fragment_layout.R;
 
-public abstract class SingleFragmentActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     protected abstract Fragment createFragment();
 
