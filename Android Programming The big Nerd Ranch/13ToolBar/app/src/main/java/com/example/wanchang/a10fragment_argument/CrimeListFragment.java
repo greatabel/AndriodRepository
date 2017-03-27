971a2a4417189d0dc6c9e26d972b3e0d3357fea8
greatabel <myreceiver2for2github@gmail.com>
2017-03-27 14:18:48 +0800

    show menu add button

--- Android Programming The big Nerd Ranch/13ToolBar/app/src/main/java/com/example/wanchang/a10fragment_argument/CrimeListFragment.java
+++ Android Programming The big Nerd Ranch/13ToolBar/app/src/main/java/com/example/wanchang/a10fragment_argument/CrimeListFragment.java
@@ -7,6 +7,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
@@ -61,6 +63,19 @@ public class CrimeListFragment extends Fragment {
         super.onResume();
         updateUI();
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_crime_list, menu);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
 
 
     private void updateUI() {
