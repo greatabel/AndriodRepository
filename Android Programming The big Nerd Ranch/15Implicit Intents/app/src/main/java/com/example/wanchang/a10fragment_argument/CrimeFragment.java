24ac1748e1f89dcca1a79197da20d231a4f39bed
greatabel <myreceiver2for2github@gmail.com>
2017-04-11 08:32:33 +0800

    comment out test filter code

--- Android Programming The big Nerd Ranch/15Implicit Intents/app/src/main/java/com/example/wanchang/a10fragment_argument/CrimeFragment.java
+++ Android Programming The big Nerd Ranch/15Implicit Intents/app/src/main/java/com/example/wanchang/a10fragment_argument/CrimeFragment.java
@@ -173,8 +173,9 @@ public class CrimeFragment extends Fragment {
 
         final Intent pickContact = new Intent(Intent.ACTION_PICK,
                 ContactsContract.Contacts.CONTENT_URI);
+        
         // 过滤器测试
-        pickContact.addCategory(Intent.CATEGORY_HOME);
+//        pickContact.addCategory(Intent.CATEGORY_HOME);
         // ---
 
         mSuspectButton = (Button)v.findViewById(R.id.crime_suspect);
