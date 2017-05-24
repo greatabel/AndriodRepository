c1443f7afde52ca034056eb52a1759c9950ba721
greatabel <myreceiver2for2github@gmail.com>
2017-05-24 10:42:27 +0800

    16CameraIntent with PictureDetail

---
+++ Android Programming The big Nerd Ranch/16CameraIntent with PictureDetail/app/src/main/java/com/example/wanchang/a10fragment_argument/CrimeLab.java
@@ -0,0 +1,146 @@
+package com.example.wanchang.a10fragment_argument;
+
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Environment;
+
+import com.example.wanchang.a10fragment_argument.CrimeDbSchema.CrimeTable;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class CrimeLab {
+    private  static CrimeLab sCrimeLab;
+//    private List<Crime> mCrimes;
+
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
+
+
+//    public List<Crime> getmCrimes() {
+//        return new ArrayList<>();
+//    }
+
+    public List<Crime> getCrimes() {
+        List<Crime> crimes = new ArrayList<>();
+
+        CrimeCursorWrapper cursor = queryCrimes(null, null);
+
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            crimes.add(cursor.getCrime());
+            cursor.moveToNext();
+        }
+        cursor.close();
+
+        return crimes;
+    }
+
+    // just to test
+//    public void test_change(int position){
+//        if (position < mCrimes.size()) {
+//            mCrimes.get(position).setmTitle("test " + position);
+//        }
+////        for(int i=0; i< 20;i++) {
+////
+////            mCrimes.get(i).setmTitle("test " + i);
+////        }
+//    }
+
+
+    public Crime getCrime(UUID id) {
+        CrimeCursorWrapper cursor = queryCrimes(
+                CrimeTable.Cols.UUID + " = ?",
+                new String[] { id.toString() }
+        );
+
+        try {
+            if (cursor.getCount() == 0) {
+                return null;
+            }
+
+            cursor.moveToFirst();
+            return cursor.getCrime();
+        } finally {
+            cursor.close();
+        }
+    }
+
+    public File getPhotoFile(Crime crime) {
+        File externalFileDir = mContext
+                .getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        if (externalFileDir == null) {
+            return null;
+        }
+        return new File(externalFileDir, crime.getPhotoFilename());
+    }
+
+
+    public static CrimeLab get(Context context) {
+        if(sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+
+    private CrimeLab(Context context) {
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
+
+//        mCrimes = new ArrayList<>();
+//        for(int i=0; i< 30;i++){
+//            Crime crime = new Crime();
+//            crime.setmTitle("缺点 #" + i);
+//            crime.setmSolved(i % 2 == 0);
+//            mCrimes.add(crime);
+//        }
+    }
+
+    public void updateCrime(Crime crime) {
+        String uuidString = crime.getmId().toString();
+        ContentValues values = getContentValues(crime);
+
+        mDatabase.update(CrimeTable.NAME, values,
+                CrimeTable.Cols.UUID + " = ?",
+                new String[] { uuidString });
+    }
+
+    public void addCrime(Crime c){
+//        mCrimes.add(c);
+        ContentValues values = getContentValues(c);
+        mDatabase.insert(CrimeTable.NAME, null , values);
+    }
+
+    private static ContentValues getContentValues(Crime crime) {
+        ContentValues values = new ContentValues();
+        values.put(CrimeTable.Cols.UUID, crime.getmId().toString());
+        values.put(CrimeTable.Cols.TITLE, crime.getmTitle());
+        values.put(CrimeTable.Cols.DATE, crime.getmDate().getTime());
+        values.put(CrimeTable.Cols.SOLVED, crime.ismSolved() ? 1 : 0);
+        values.put(CrimeTable.Cols.SUSPECT, crime.getmSuspect());
+
+        return values;
+    }
+
+    private CrimeCursorWrapper queryCrimes(String whereClause, String[] whereArgs) {
+        Cursor cursor = mDatabase.query(
+                CrimeTable.NAME,
+                null, // Columns - null selects all columns
+                whereClause,
+                whereArgs,
+                null, // groupBy
+                null, // having
+                null  // orderBy
+        );
+
+        return new CrimeCursorWrapper(cursor);
+    }
+
+
+
+}
---
+++ Android Programming The big Nerd Ranch/16CameraIntent with PictureDetail/app/src/main/java/com/example/wanchang/a10fragment_argument/CrimeListActivity.java
@@ -0,0 +1,11 @@
+package com.example.wanchang.a10fragment_argument;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+
+}
