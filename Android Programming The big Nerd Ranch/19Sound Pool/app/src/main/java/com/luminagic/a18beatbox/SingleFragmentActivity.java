de187e71e7d8efdd495f3ab7ea9fb268a3be3a2c
greatabel <myreceiver2for2github@gmail.com>
2017-06-09 14:07:13 +0800

    19Sound Pool/

---
+++ Android Programming The big Nerd Ranch/19Sound Pool/app/src/main/java/com/luminagic/a18beatbox/SingleFragmentActivity.java
@@ -0,0 +1,36 @@
+package com.luminagic.a18beatbox;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.app.Fragment;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+    @LayoutRes
+    protected  int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_fragment);
+        setContentView(getLayoutResId());
+
+        FragmentManager fm = getFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+
+}
