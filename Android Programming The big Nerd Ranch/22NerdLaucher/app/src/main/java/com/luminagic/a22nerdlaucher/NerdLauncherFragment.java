224b266319c78c329bd15f963d28ae67baef01c1
greatabel <myreceiver2for2github@gmail.com>
2017-08-06 20:40:46 +0800

    setupAdpater

--- Android Programming The big Nerd Ranch/22NerdLaucher/app/src/main/java/com/luminagic/a22nerdlaucher/NerdLauncherFragment.java
+++ Android Programming The big Nerd Ranch/22NerdLaucher/app/src/main/java/com/luminagic/a22nerdlaucher/NerdLauncherFragment.java
@@ -1,15 +1,23 @@
 package com.luminagic.a22nerdlaucher;
 
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Bundle;
 
+import java.util.List;
+
 public class NerdLauncherFragment extends Fragment {
+
+    private  static final String TAG = "NerdLauncherFragment";
 
     private RecyclerView mRecyclerView;
 
@@ -24,7 +32,25 @@ public class NerdLauncherFragment extends Fragment {
         mRecyclerView = (RecyclerView) v
                             .findViewById(R.id.fragment_nerd_launcher_recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        setupAdapter();
         return v;
     }
+
+    private void setupAdapter() {
+        Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
+
+        Log.i(TAG, "Found " + activities.size() + " activities.");
+    }
+
+
+
+
+
+
+
 
 }
