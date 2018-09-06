0de795c3c84a32392ed68849df68b7196cf9dc8c
greatabel <myreceiver2for2github@gmail.com>
2018-09-06 16:11:49 +0800

    landscape

--- MeomoDemo/12localBrowser_2webviews/app/src/main/java/com/luminagic/wanchang/a12localbrowser/MainActivity.java
+++ MeomoDemo/12localBrowser_2webviews/app/src/main/java/com/luminagic/wanchang/a12localbrowser/MainActivity.java
@@ -21,8 +21,9 @@ public class MainActivity extends Activity {
     private static final String TAG = "LocalBrowser";
     private final Handler handler = new Handler();
     private WebView webView;
-    private TextView textView;
-    private Button button;
+    private WebView webViewR;
+//    private TextView textView;
+//    private Button button;
 
     private class AndroidBridge {
         @JavascriptInterface
@@ -31,7 +32,7 @@ public class MainActivity extends Activity {
                 @Override
                 public void run() {
                     Log.d(TAG, "callAndroid" + arg + ")");
-                    textView.setText(arg);
+//                    textView.setText(arg);
                 }
             });
         }
@@ -49,8 +50,9 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         webView = (WebView)findViewById(R.id.web_view);
-        textView = (TextView)findViewById(R.id.text_view);
-        button = (Button)findViewById(R.id.button);
+        webViewR = (WebView)findViewById(R.id.web_viewR);
+//        textView = (TextView)findViewById(R.id.text_view);
+//        button = (Button)findViewById(R.id.button);
 
         //Turn on Javascript in the embedded browser
         webView.getSettings().setJavaScriptEnabled(true);
@@ -66,33 +68,50 @@ public class MainActivity extends Activity {
         // load the web page from a local asset
         webView.loadUrl("file:///android_asset/local/ST/differentGirl/index.html");
 
+        //---------第二个webview------
+        //Turn on Javascript in the embedded browser
+        webViewR.getSettings().setJavaScriptEnabled(true);
+
+
+        //调用JS方法.安卓版本大于17,加上注解 @JavascriptInterface
+        webViewR.getSettings().setJavaScriptEnabled(true);
+        webViewR.getSettings().setAllowFileAccess(true);
+        webViewR.getSettings().setAllowFileAccessFromFileURLs(true);
+        webViewR.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        webViewR.getSettings().setSupportMultipleWindows(true);
+
+        // load the web page from a local asset
+        webViewR.loadUrl("file:///android_asset/local/ST/differentGirl/index.html");
+
+
+        //--------------------------
         // expose a Java object to javascript in the browser
         webView.addJavascriptInterface(new AndroidBridge(),"android");
 
         // Set up a function to be called when JavaScript tries
         // to open an alert window
-        webView.setWebChromeClient(new WebChromeClient(){
-            @Override
-            public boolean onJsAlert(final WebView view,
-                                     final  String url, final  String message, JsResult result) {
-                Log.d(TAG, "onJsAlert(" + view + ", " + url + ", "
-                        + message + ", " + result + ")");
-                Toast.makeText(MainActivity.this, message+" @-@", Toast.LENGTH_LONG).show();
-                result.confirm();
-                return true; // I handled it
-            }
-        });
+//        webView.setWebChromeClient(new WebChromeClient(){
+//            @Override
+//            public boolean onJsAlert(final WebView view,
+//                                     final  String url, final  String message, JsResult result) {
+//                Log.d(TAG, "onJsAlert(" + view + ", " + url + ", "
+//                        + message + ", " + result + ")");
+//                Toast.makeText(MainActivity.this, message+" @-@", Toast.LENGTH_LONG).show();
+//                result.confirm();
+//                return true; // I handled it
+//            }
+//        });
 
-        button.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "onClick(" + v + ")");
-                //note a single Random object is reused here
-                Random randomGenerator = new Random();
-                int randomInt = randomGenerator.nextInt(1000);
-                webView.loadUrl("javascript:callJS('Hello from Android."+randomInt+ "')");
-            }
-        });
+//        button.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d(TAG, "onClick(" + v + ")");
+//                //note a single Random object is reused here
+//                Random randomGenerator = new Random();
+//                int randomInt = randomGenerator.nextInt(1000);
+//                webView.loadUrl("javascript:callJS('Hello from Android."+randomInt+ "')");
+//            }
+//        });
     }
 
 
