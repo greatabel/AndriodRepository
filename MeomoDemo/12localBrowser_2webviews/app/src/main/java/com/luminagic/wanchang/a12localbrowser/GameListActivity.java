72c9ce57ddb27afad7e4da9a6a45e988393e8b34
greatabel <myreceiver2for2github@gmail.com>
2018-09-07 12:05:40 +0800

    list

---
+++ MeomoDemo/12localBrowser_2webviews/app/src/main/java/com/luminagic/wanchang/a12localbrowser/GameListActivity.java
@@ -0,0 +1,18 @@
+package com.luminagic.wanchang.a12localbrowser;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+public class GameListActivity extends ListActivity {
+
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, values);
+        setListAdapter(adapter);
+    }
+}
