363d4123aa87595a7b07987048be935f0bdc7196
greatabel <myreceiver2for2github@gmail.com>
2020-04-12 14:46:51 +0800

    dart name

--- Flutter in Action/i0hello.dart
+++ Flutter in Action/i0hello.dart
@@ -1,3 +1,13 @@
 void main() {
     print('Hello, Dart from abel!!');
+    helloDart();
+    helloDartA('test');
+}
+
+void helloDart() {
+    print('Hello, Dart!');
+}
+
+void helloDartA(String name) {
+    print('Hllo, $name !');
 }
