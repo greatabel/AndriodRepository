7f2a539a86dc8a923af6b9caf6a1d4212affc1b2
greatabel <myreceiver2for2github@gmail.com>
2020-04-16 17:31:06 +0800

    listview

--- flutter-book/i3widgets/i3container/lib/list_demo.dart
+++ flutter-book/i3widgets/i3container/lib/list_demo.dart
@@ -7,12 +7,75 @@ class ListDemo extends StatelessWidget {
       appBar: AppBar(
         title: Text('Text组件示例'),
       ),
-      body: new ListView(
+      body: Column(
         children: <Widget>[
-          ListTile(leading: Icon(Icons.alarm), title: Text("alarm")),
-          ListTile(leading: Icon(Icons.phone), title: Text("phone")),
-          ListTile(leading: Icon(Icons.airplay), title: Text("airplay")),
-        ],
+          Expanded(child:new ListView(
+            children: <Widget>[
+              ListTile(leading: Icon(Icons.alarm), title: Text("alarm")),
+              ListTile(leading: Icon(Icons.phone), title: Text("phone")),
+              ListTile(leading: Icon(Icons.airplay), title: Text("airplay")),
+            ],
+          )),
+          Expanded(child:Container(
+            //顶部外边距为20
+            margin: EdgeInsets.symmetric(vertical: 20.0),
+            //设定容器高度
+            height: 200.0,
+            child: ListView(
+              //设置水平方向排列
+              scrollDirection: Axis.horizontal,
+              //添加子元素
+              children: <Widget>[
+                //每个Container即为一个列表项
+                Container(
+                  width: 160.0,
+                  color: Colors.lightBlue,
+                ),
+                Container(
+                  width: 160.0,
+                  color: Colors.amber,
+                ),
+                //此容器里"水平"及"列表"文字为垂直布局
+                Container(
+                  width: 160.0,
+                  color: Colors.green,
+                  //垂直布局
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        '水平',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 36.0,
+                        ),
+                      ),
+                      Text(
+                        '列表',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 36.0,
+                        ),
+                      ),
+                      Icon(Icons.list),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: 160.0,
+                  color: Colors.deepPurpleAccent,
+                ),
+                Container(
+                  width: 160.0,
+                  color: Colors.black,
+                ),
+                Container(
+                  width: 160.0,
+                  color: Colors.pinkAccent,
+                ),
+              ],
+            ),
+          ),)
+        ]
       )
     );
   }
