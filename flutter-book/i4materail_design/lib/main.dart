d9318061c822408a95b7dd4cea912e6d5cdccc7c
greatabel <myreceiver2for2github@gmail.com>
2020-04-17 09:38:29 +0800

    router and dock

--- flutter-book/i4materail_design/lib/main.dart
+++ flutter-book/i4materail_design/lib/main.dart
@@ -9,6 +9,12 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       home: new MyHomePage(),
       title: 'MaterialApp例子',
+      routes: {
+        '/first': (BuildContext context) => FirstPage(),
+        '/second': (BuildContext context) => SecondPage(),
+      },
+      initialRoute: '/first',
+      theme: new ThemeData(primarySwatch: Colors.red),
     );
   }
 }
@@ -23,8 +29,54 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(title: Text('例子')),
-      body: Center(child: Text('主页')),
+      body: Center(child: Text('Scaffold脚手架')),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(height: 50.0,),
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed:  () {},
+        tooltip: '增加',
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
 
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(title: Text('这是第1页')),
+      body: Center(
+        child: RaisedButton(
+          child: Text('这是第1页内容，点我可以跳转'),
+          onPressed: (){
+            Navigator.pushNamed(context, '/second');
+          },
+        ),
+      )
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        appBar: AppBar(title: Text('这是第2页')),
+        body: Center(
+          child: RaisedButton(
+            child: Text('这是第2页内容, 点我可以跳转'),
+            onPressed: (){
+              Navigator.pushNamed(context, '/first');
+            },
+          ),
+        )
+    );
   }
 }
