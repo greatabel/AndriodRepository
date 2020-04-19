7aff4ee84d45d6f9aacd560871d78207eb6d0b85
greatabel <myreceiver2for2github@gmail.com>
2020-04-19 18:11:42 +0800

    wrap

--- flutter-book/i5layout/lib/offstage_page.dart
+++ flutter-book/i5layout/lib/offstage_page.dart
@@ -27,19 +27,47 @@ class _OffStagePage extends State<OffStagePage>{
       ),
     );
 
+    var wrapW = Wrap(
+      spacing: 8.0, // Chip之间的间距大小
+      runSpacing: 4.0, // 行之间的间距大小
+      children: <Widget>[
+        Chip(
+          //添加圆形头像
+          avatar: CircleAvatar(
+              backgroundColor: Colors.lightGreen.shade800, child: Text('西门', style: TextStyle(fontSize: 10.0),)),
+          label: Text('西门吹雪'),
+        ),
+        Chip(
+          avatar: CircleAvatar(
+              backgroundColor: Colors.lightBlue.shade700, child: Text('司空', style: TextStyle(fontSize: 10.0),)),
+          label: Text('司空摘星'),
+        ),
+        Chip(
+          avatar: CircleAvatar(
+              backgroundColor: Colors.orange.shade800, child: Text('婉清', style: TextStyle(fontSize: 10.0),)),
+          label: Text('木婉清'),
+        ),
+        Chip(
+          avatar: CircleAvatar(
+              backgroundColor: Colors.blue.shade900, child: Text('一郎', style: TextStyle(fontSize: 10.0),)),
+          label: Text('萧十一郎'),
+        ),
+      ],
+    );
     return new Scaffold(
         appBar: AppBar(title: Text('Stack填充布局')),
-        body: new Center(
-          child: Container(
+        body:  Container(
+            width: MediaQuery.of(context).size.width,
             decoration: BoxDecoration(
             gradient: RadialGradient(colors: [
-            Colors.grey,
+            Colors.blue,
             Colors.pink,
-            ], radius: 0.85, focal: Alignment.center),
+            ], radius: 0.95, focal: Alignment.center),
             ),
-            child: offstageW,
+            child: Column(
+              children: <Widget>[offstageW,wrapW],
+            ),
           ),
-        ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
           //设置是否显示文本组件
