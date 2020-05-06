ca85400c98606af9ee59b1c1c4611c3fd84e24f2
greatabel <myreceiver2for2github@gmail.com>
2020-05-06 17:16:09 +0800

    loading dart

---
+++ flutter-book/ch16im/lib/loading.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class LoadingPage extends StatefulWidget {
+  @override
+  _LoadingState createState() => new _LoadingState();
+}
+
+class _LoadingState extends State<LoadingPage> {
+  @override
+  void initState() {
+    super.initState();
+    new Future.delayed(Duration(seconds: 3), (){
+      print("Flutter app ui 实现...");
+      Navigator.of(context).pushReplacementNamed("app");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Center(
+      child: Stack(
+        children: <Widget>[
+          Image.asset("images/loading.jpeg", fit: BoxFit.cover,),
+        ],
+      ),
+    );
+  }
+}
