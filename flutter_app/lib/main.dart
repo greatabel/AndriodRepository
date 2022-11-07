f2ebc683e1b8f78810b0b95c04e3fa42eb417bd0
greatabel <myreceiver2for2github@gmail.com>
2022-11-07 23:25:10 +0800

    finish list

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -8,13 +8,47 @@ class RandomWords extends StatefulWidget {
   State<RandomWords> createState() => _RandomWordsState();
 }
 
+// class _RandomWordsState extends State<RandomWords> {
+//   @override
+//   Widget build(BuildContext context) {
+//     final wordPair = WordPair.random();
+//     return Text(wordPair.asPascalCase);
+//   }
+// }
 class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+  // #enddocregion RWS-var
+
+  // #docregion RWS-build
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    // #docregion itemBuilder
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: /*1*/ (context, i) {
+        if (i.isOdd) return const Divider(); /*2*/
+
+        final index = i ~/ 2; /*3*/
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+        }
+        // #docregion listTile
+        return ListTile(
+          title: Text(
+            _suggestions[index].asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+        // #enddocregion listTile
+      },
+    );
+    // #enddocregion itemBuilder
   }
+// #enddocregion RWS-build
+// #docregion RWS-var
 }
+// #enddocregion RWS-var
 
 void main() {
   runApp(const MyApp());
@@ -27,10 +61,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // final wordPair = WordPair.random();
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Startup Name Generator',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: const Text('Startup Name Generator I'),
         ),
         body: const Center(
           // child: Text(wordPair.asPascalCase),
