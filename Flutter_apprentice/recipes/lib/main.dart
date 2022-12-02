8e7ee36b81bddc6f7c0d0145eb42c61dc3b18591
greatabel <myreceiver2for2github@gmail.com>
2022-12-02 20:36:15 +0800

    refactor RecipeApp

--- Flutter_apprentice/recipes/lib/main.dart
+++ Flutter_apprentice/recipes/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const RecipeApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class RecipeApp extends StatelessWidget {
+  const RecipeApp({super.key});
 
   // This widget is the root of your application.
   @override
