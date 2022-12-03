996812b80fa671fad6438af1fbab44df16ed4a38
greatabel <myreceiver2for2github@gmail.com>
2022-12-03 23:17:19 +0800

    card style

--- Flutter_apprentice/recipes/lib/main.dart
+++ Flutter_apprentice/recipes/lib/main.dart
@@ -51,19 +51,37 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   Widget buildRecipeCard(Recipe recipe) {
-  // 1
-  return Card(
-  // 2
-  child: Column(
-  // 3
-  children: <Widget>[
-  // 4
-  Image(image: AssetImage(recipe.imageUrl)),
-  // 5
-    Text(recipe.label),
-      ],
+
+    return Card(
+    // 1
+    elevation: 2.0,
+    // 2
+    shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(10.0)),
+    // 3
+      child: Padding(
+      padding: const EdgeInsets.all(16.0),
+    // 4
+        child: Column(
+    children: <Widget>[
+    Image(image: AssetImage(recipe.imageUrl)),
+    // 5
+    const SizedBox(
+    height: 14.0,
     ),
-   );
+    // 6
+    Text(
+    recipe.label,
+    style: const TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Palatino',
+    ),
+    )
+    ],
+    ),
+    ),
+    );
   }
 
   @override
