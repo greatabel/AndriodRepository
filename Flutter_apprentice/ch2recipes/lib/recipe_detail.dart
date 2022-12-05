6f11a4860c64828b9463a02bda8b306d9c64ceea
greatabel <myreceiver2for2github@gmail.com>
2022-12-05 17:38:47 +0800

    with slider

--- Flutter_apprentice/ch2recipes/lib/recipe_detail.dart
+++ Flutter_apprentice/ch2recipes/lib/recipe_detail.dart
@@ -14,6 +14,8 @@ class RecipeDetail extends StatefulWidget {
 
 class _RecipeDetailState extends State<RecipeDetail> {
 
+  int _sliderVal = 1;
+
 @override
 Widget build(BuildContext context) {
   // 1
@@ -57,6 +59,23 @@ Widget build(BuildContext context) {
               },
             ),
           ),
+
+          Slider(
+            min:1,
+            max: 10,
+            divisions: 10,
+            label: '${_sliderVal * widget.recipe.servings} servings',
+            value: _sliderVal.toDouble(),
+            onChanged: (newVaue) {
+              setState(() {
+                _sliderVal = newVaue.round();
+              });
+            },
+            activeColor: Colors.green,
+            inactiveColor: Colors.black,
+          ),
+
+
         ],
       ),
     ),
