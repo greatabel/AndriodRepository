9bf4b504168b2f1643fe3d99e35b9a0500937680
greatabel <myreceiver2for2github@gmail.com>
2022-12-06 10:42:18 +0800

    ch03 start

---
+++ Flutter_apprentice/ch03widgets/android/app/src/main/kotlin/com/example/ch03widgets/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ch03widgets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Flutter_apprentice/ch03widgets/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // 1
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({Key? key}) : super(key: key);
+  // 2
+  @override
+  Widget build(BuildContext context) {
+    // 3
+    return MaterialApp(
+      title: 'Fooderlich',
+      // 4
+      home: Scaffold(
+        // 5
+        appBar: AppBar(title: const Text('Fooderlich')),
+        body: const Center(child: Text('Let\'s get cooking 👩‍🍳')),
+      ),
+    );
+  }
+}
